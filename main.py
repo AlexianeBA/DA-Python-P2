5515a67af54c586d15f16a24fb9dc45faa83a5c3
AlexianeBA <barbealexiane07@gmail.com>
2023-09-17 20:34:38 +0200

    create pagination

--- main.py
+++ main.py
@@ -8,6 +8,24 @@ try:
 except:
     print("ko")
 
+
+
+# TO DO : comment passer à la page suivante (pagination)
+def list_all_pages_of_category():
+    list_url_category = []
+    page_next = 1 
+    while True:
+        url = f"http://books.toscrape.com/catalogue/category/books/mystery_3/page-{page_next}.html"
+        reponse = requests.get(url)
+        page = reponse.status_code
+        print(page)
+        if page == 200:
+            list_url_category.append(url)
+            print(url)
+            page_next +=1
+        else:
+            break
+    return list_url_category
 # CSV of one book
 def to_csv(datas, category):
    
@@ -23,29 +41,29 @@ def scrapp_page_one_book(url_book_of_one_book):
     table_livre=[]
     # url
     product_page_url = url_book_of_one_book
-    print(product_page_url)
+    
 
     # UPC
     for td in soup.find_all('td'):
         table_livre.append(td)
     upc= table_livre[0].text.strip()
-    print(upc)
+    
 
     # title
     title = soup.find('h1').string
-    print(title)
+    
 
     # price avec tax
     price_including_tax= table_livre[2].text.strip()
-    print(price_including_tax)
+   
 
     # price sans tax
     price_excluding_tax = table_livre[3].text.strip()
-    print(price_excluding_tax)
+    
 
     #number_available
     number_available = table_livre[5].text.strip()
-    print(number_available)
+    
 
     # product_description
     div_product_description = soup.find("article", class_="product_page")
@@ -53,7 +71,7 @@ def scrapp_page_one_book(url_book_of_one_book):
 
     if p_product_description:
         prodcut_description = p_product_description[3].get_text()
-        print(prodcut_description)
+        
     else:
         print("aucune desciption trouvée")
 
@@ -62,19 +80,19 @@ def scrapp_page_one_book(url_book_of_one_book):
     for a_href in soup.find_all("a"):
         list_a.append(a_href.string)
     category = list_a[3]
-    print(category)
+    
 
 
     # review_rating
     review_rating = table_livre[6].text.strip()
-    print(review_rating)
+    
 
 
     # img_rul
     img = soup.find_all('img')
     for image in img:
         image_url = image['src']
-    print(image_url.replace("../../", "http://books.toscrape.com/" ))
+        image_url.replace("../../", "http://books.toscrape.com/" )
     
     
     datas_of_one_book = [product_page_url, upc, title, price_including_tax, price_excluding_tax, number_available,  prodcut_description, category, review_rating, image_url]
@@ -87,28 +105,28 @@ url_book_of_one_book = "http://books.toscrape.com/catalogue/a-light-in-the-attic
 
 
 # scrapp all URL of books in mystery categorie 
-def links_books_of_category_mistery(url_page_category):
-    reponse = requests.get(url_page_category)
-    page = reponse.content
-    soup = BeautifulSoup(page, 'html.parser')
+def links_books_of_category_mistery(list_url_category):
     datas = []
-    
-    for values in soup.find_all("h3"):
-        a = values.find("a")
-        href = a['href']
-        a_href_remplaced = href.replace("../../..", "http://books.toscrape.com/catalogue")
-        print(a_href_remplaced)
-        datas_of_one_book, category = scrapp_page_one_book(a_href_remplaced)
-        
-        datas.append(datas_of_one_book) 
+    for url_page_category in list_url_category:
+        print(url_page_category)
+        reponse = requests.get(url_page_category)
+        page = reponse.content
+        soup = BeautifulSoup(page, 'html.parser')            
+        for values in soup.find_all("h3"):
+            a = values.find("a")
+            href = a['href']
+            a_href_remplaced = href.replace("../../..", "http://books.toscrape.com/catalogue")
+            datas_of_one_book, category = scrapp_page_one_book(a_href_remplaced)
+            datas.append(datas_of_one_book) 
     to_csv(datas, category)
-    
-url_page_category = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
-links_books_of_category_mistery(url_page_category)
 
-print("*****")
+list_url_category = list_all_pages_of_category()
+links_books_of_category_mistery(list_url_category)
 
-# TO DO : comment passer à la page suivante (pagination)
+
+
+
+
 # Scrap all URL of category in home page
 def links_categories_home_page(url):
     reponse = requests.get(url)
@@ -126,4 +144,3 @@ url = "http://books.toscrape.com/index.html"
 links_categories_home_page(url)
 
 print("*****")
-
